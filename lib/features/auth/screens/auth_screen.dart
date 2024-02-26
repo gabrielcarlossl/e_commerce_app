8fc9c5a3bd497a0289d01ed4a5cc7072965a9b60
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-02-26 20:58:43 -0300

    project init

---
+++ lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart'
+
+class AuthScree extends StatefulWidget {
+  const AuthScree({super.key});
+
+  @override
+  State<AuthScree> createState() => _AuthScreeState();
+}
+
+class _AuthScreeState extends State<AuthScree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
